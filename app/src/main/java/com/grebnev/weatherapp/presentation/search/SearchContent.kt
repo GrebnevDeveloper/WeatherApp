3fd5c734e1f904dbe84368e1d709560e5da1cf5b
grebnev <grebnev.developer@gmail.com>
2024-12-27 17:46:27 +0400

    Completed implementation screen SearchContent.kt

--- app/src/main/java/com/grebnev/weatherapp/presentation/search/SearchContent.kt
+++ app/src/main/java/com/grebnev/weatherapp/presentation/search/SearchContent.kt
@@ -1,8 +1,156 @@
 package com.grebnev.weatherapp.presentation.search
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.grebnev.weatherapp.R
+import com.grebnev.weatherapp.domain.entity.City
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchContent(component: SearchComponent) {
+    val state by component.model.collectAsState()
+    val focusRequester = remember {
+        FocusRequester()
+    }
 
+    var expanded by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(key1 = Unit) {
+        focusRequester.requestFocus()
+    }
+
+    SearchBar(
+        modifier = Modifier.focusRequester(focusRequester),
+        inputField = {
+            SearchBarDefaults.InputField(
+                query = state.searchQuery,
+                onQueryChange = { component.changedSearchQuery(it) },
+                onSearch = { component.onSearchClick() },
+                expanded = expanded,
+                onExpandedChange = { expanded = it },
+                placeholder = { Text(text = stringResource(R.string.text_enter_city_name)) },
+                leadingIcon = {
+                    IconButton(onClick = { component.onBackClick() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
+                },
+                trailingIcon = {
+                    IconButton(onClick = { component.onSearchClick() }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+        },
+        expanded = expanded,
+        onExpandedChange = { component.onBackClick() },
+    ) {
+        when (val searchState = state.searchState) {
+            SearchStore.State.SearchState.EmptyResult -> {
+                Text(text = stringResource(R.string.text_nothing_found))
+            }
+
+            SearchStore.State.SearchState.Error -> {
+                Text(text = stringResource(R.string.text_something_went_wrong))
+            }
+
+            SearchStore.State.SearchState.Initial -> {
+
+            }
+
+            SearchStore.State.SearchState.Loading -> {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.align(Alignment.Center),
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+
+            is SearchStore.State.SearchState.SuccessLoaded -> {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+                    items(
+                        items = searchState.cities,
+                        key = { it.id }
+                    ) { city ->
+                        CityCard(
+                            city = city,
+                            onCityClick = { component.onCityClick(it) }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CityCard(
+    city: City,
+    onCityClick: (City) -> Unit
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 16.dp)
+                .clickable { onCityClick(city) }
+        ) {
+            Text(
+                text = city.name,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = city.country)
+        }
+    }
 }
