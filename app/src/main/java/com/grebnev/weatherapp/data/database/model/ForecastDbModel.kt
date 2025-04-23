9de18c67d04461eccf5b51cd0ecf2a3ef6b81e33
grebnev <grebnev.developer@gmail.com>
2025-04-23 18:53:18 +0400

    Added cascade deletion forecast and weather

--- app/src/main/java/com/grebnev/weatherapp/data/database/model/ForecastDbModel.kt
+++ app/src/main/java/com/grebnev/weatherapp/data/database/model/ForecastDbModel.kt
@@ -1,11 +1,22 @@
 package com.grebnev.weatherapp.data.database.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
 
 @Serializable
-@Entity(tableName = "forecast")
+@Entity(
+    tableName = "forecast",
+    foreignKeys = [
+        ForeignKey(
+            entity = CityDbModel::class,
+            parentColumns = ["id"],
+            childColumns = ["cityId"],
+            onDelete = ForeignKey.CASCADE,
+        ),
+    ],
+)
 data class ForecastDbModel(
     @PrimaryKey val cityId: Long,
     val currentWeather: WeatherDbModel,
--- app/src/main/java/com/grebnev/weatherapp/data/database/model/WeatherDbModel.kt
+++ app/src/main/java/com/grebnev/weatherapp/data/database/model/WeatherDbModel.kt
@@ -1,14 +1,26 @@
 package com.grebnev.weatherapp.data.database.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
 
 @Serializable
-@Entity(tableName = "weathers")
+@Entity(
+    tableName = "weathers",
+    foreignKeys = [
+        ForeignKey(
+            entity = ForecastDbModel::class,
+            parentColumns = ["cityId"],
+            childColumns = ["forecastCityId"],
+            onDelete = ForeignKey.CASCADE,
+        ),
+    ],
+)
 data class WeatherDbModel(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
+    val forecastCityId: Long,
     val tempC: Float,
     val conditionText: String,
     val conditionUrl: String,
